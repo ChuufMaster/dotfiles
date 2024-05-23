eeaca6e563526420e10622a1f2b43326ac42b98a
ChuufMaster <ivanhorak20@gmail.com>
2024-05-23 14:38:17 +0200

    Added a fugitive binding to stage file, make a commit message and push

--- lua/ChuufMaster/plugins/fugitive.lua
+++ lua/ChuufMaster/plugins/fugitive.lua
@@ -4,9 +4,16 @@ return {
   event = 'VimEnter',
   config = function()
     vim.keymap.set('n', '<leader>gs', function()
-      vim.cmd 'Git add .'
-      local input = vim.fn.input 'Commit Message: '
+      vim.cmd('Git add .')
+      local input = vim.fn.input('Commit Message: ')
       vim.cmd('Git commit -m"' .. input .. '"')
     end, { desc = 'Stage all files and write commit message' })
+
+    vim.keymap.set('n', '<leader>gS', function()
+      vim.cmd('Git add .')
+      local input = vim.fn.input('Commit Message: ')
+      vim.cmd('Git commit -m"' .. input .. '"')
+      vim.cmd('Git push')
+    end, { desc = 'Stage all files, add a commit message and push' })
   end,
 }
