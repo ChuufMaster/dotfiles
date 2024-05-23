6e7848e12964a1cb17438e1bf3d94dd92dae5791
ChuufMaster <ivanhorak20@gmail.com>
2024-05-23 14:28:23 +0200

    Added the fugitive command to stage and push all files

--- lua/ChuufMaster/plugins/fugitive.lua
+++ lua/ChuufMaster/plugins/fugitive.lua
@@ -1,7 +1,12 @@
 return {
   'tpope/vim-fugitive',
   lazy = false,
-  -- config = function()
-  --   require('core.utils').load_mappings 'fugitive'
-  -- end,
+  event = 'VimEnter',
+  config = function()
+    vim.keymap.set('n', '<leader>gs', function()
+      vim.cmd 'Git add .'
+      local input = vim.fn.input 'Commit Message: '
+      vim.cmd('Git commit -m"' .. input .. '"')
+    end, { desc = 'Stage all files and write commit message' })
+  end,
 }
