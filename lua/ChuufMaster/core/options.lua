4639d77b382cc67ab965c27220275a00ad4005f6
ChuufMaster <ivanhorak20@gmail.com>
2024-05-24 10:11:59 +0200

    Fixes and making lualine work instead of mini's status line

--- lua/ChuufMaster/core/options.lua
+++ lua/ChuufMaster/core/options.lua
@@ -84,17 +84,17 @@ end
 
 -- Check if we need to reload the file when it changed
 vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
-  group = augroup 'checktime',
+  group = augroup('checktime'),
   callback = function()
     if vim.o.buftype ~= 'nofile' then
-      vim.cmd 'checktime'
+      vim.cmd('checktime')
     end
   end,
 })
 
 -- Highlight on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = augroup 'highlight_yank',
+  group = augroup('highlight_yank'),
   callback = function()
     vim.highlight.on_yank()
   end,
@@ -102,17 +102,17 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- resize splits if window got resized
 vim.api.nvim_create_autocmd({ 'VimResized' }, {
-  group = augroup 'resize_splits',
+  group = augroup('resize_splits'),
   callback = function()
     local current_tab = vim.fn.tabpagenr()
-    vim.cmd 'tabdo wincmd ='
+    vim.cmd('tabdo wincmd =')
     vim.cmd('tabnext ' .. current_tab)
   end,
 })
 
 -- go to last loc when opening a buffer
 vim.api.nvim_create_autocmd('BufReadPost', {
-  group = augroup 'last_loc',
+  group = augroup('last_loc'),
   callback = function(event)
     local exclude = { 'gitcommit' }
     local buf = event.buf
@@ -130,7 +130,7 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 
 -- close some filetypes with <q>
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup 'close_with_q',
+  group = augroup('close_with_q'),
   pattern = {
     'PlenaryTestPopup',
     'help',
@@ -153,7 +153,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- make it easier to close man-files when opened inline
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup 'man_unlisted',
+  group = augroup('man_unlisted'),
   pattern = { 'man' },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
@@ -162,7 +162,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup 'wrap_spell',
+  group = augroup('wrap_spell'),
   pattern = { 'gitcommit', 'markdown' },
   callback = function()
     vim.opt_local.wrap = true
@@ -172,7 +172,7 @@ vim.api.nvim_create_autocmd('FileType', {
 
 -- Fix conceallevel for json files
 vim.api.nvim_create_autocmd({ 'FileType' }, {
-  group = augroup 'json_conceal',
+  group = augroup('json_conceal'),
   pattern = { 'json', 'jsonc', 'json5' },
   callback = function()
     vim.opt_local.conceallevel = 0
@@ -181,9 +181,9 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
 
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-  group = augroup 'auto_create_dir',
+  group = augroup('auto_create_dir'),
   callback = function(event)
-    if event.match:match '^%w%w+:[\\/][\\/]' then
+    if event.match:match('^%w%w+:[\\/][\\/]') then
       return
     end
     local file = vim.uv.fs_realpath(event.match) or event.match
