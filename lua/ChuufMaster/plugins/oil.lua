4eb17d112f12d68616143f2173f6d55cc90cfe19
ChuufMaster <ivanhorak20@gmail.com>
2024-05-29 12:09:27 +0200

    Congigured oil

--- lua/ChuufMaster/plugins/oil.lua
+++ lua/ChuufMaster/plugins/oil.lua
@@ -6,14 +6,24 @@ return {
   config = function()
     require('oil').setup({
       default_file_explorer = true,
+      ['<C-h>'] = false,
+      ['<C-s>'] = false,
+      view_options = {
+        show_hidden = true,
+      },
     })
+    vim.keymap.set('n', '<leader>-', require('oil').toggle_float, { desc = 'Open Oil in a float' })
   end,
-  -- keys = {
-  --   {
-  --     '-',
-  --     function ()
-  --       vim.cmd
-  --     end
-  --   }
-  -- }
+  keys = {
+    {
+      '-',
+      '<CMD>Oil<CR>',
+      desc = 'Open Oil in parent directory',
+    },
+    -- {
+    --   '<leader>-',
+    --   require('oil').toggle_float(),
+    --   desc = 'Open oil as a float',
+    -- },
+  },
 }
