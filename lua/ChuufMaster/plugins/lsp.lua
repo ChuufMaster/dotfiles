9752e5e09ec6f1af9d03d216adef9a8cde9c3ccf
ChuufMaster <ivanhorak20@gmail.com>
2024-07-31 22:07:29 +0200

    added neotest and stuff for dotnet testing

--- lua/ChuufMaster/plugins/lsp.lua
+++ lua/ChuufMaster/plugins/lsp.lua
@@ -146,6 +146,8 @@ return { -- LSP Configuration & Plugins
       },
       markdownlint = {},
       omnisharp = {},
+      omnisharp_mono = {},
+      -- csharp_ls = {},
       pylsp = {},
       -- solargraph = {},
       yamlls = {},
@@ -200,6 +202,49 @@ return { -- LSP Configuration & Plugins
       filetypes = { 'prolog' },
       -- root directory detection for detecting the project root
       root_dir = require('lspconfig.util').root_pattern('pack.pl'),
+    })
+
+    require('lspconfig').omnisharp.setup({
+      cmd = { 'dotnet', '/home/chuufmaster/.local/share/nvim/mason/bin/omnisharp' },
+
+      settings = {
+        FormattingOptions = {
+          -- Enables support for reading code style, naming convention and analyzer
+          -- settings from .editorconfig.
+          EnableEditorConfigSupport = true,
+          -- Specifies whether 'using' directives should be grouped and sorted during
+          -- document formatting.
+          OrganizeImports = nil,
+        },
+        MsBuild = {
+          -- If true, MSBuild project system will only load projects for files that
+          -- were opened in the editor. This setting is useful for big C# codebases
+          -- and allows for faster initialization of code navigation features only
+          -- for projects that are relevant to code that is being edited. With this
+          -- setting enabled OmniSharp may load fewer projects and may thus display
+          -- incomplete reference lists for symbols.
+          LoadProjectsOnDemand = nil,
+        },
+        RoslynExtensionsOptions = {
+          -- Enables support for roslyn analyzers, code fixes and rulesets.
+          EnableAnalyzersSupport = nil,
+          -- Enables support for showing unimported types and unimported extension
+          -- methods in completion lists. When committed, the appropriate using
+          -- directive will be added at the top of the current file. This option can
+          -- have a negative impact on initial completion responsiveness,
+          -- particularly for the first few completion sessions after opening a
+          -- solution.
+          EnableImportCompletion = nil,
+          -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+          -- true
+          AnalyzeOpenDocumentsOnly = nil,
+        },
+        Sdk = {
+          -- Specifies whether to include preview versions of the .NET SDK when
+          -- determining which version to use for project loading.
+          IncludePrereleases = true,
+        },
+      },
     })
     require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
     require('mason-lspconfig').setup({
