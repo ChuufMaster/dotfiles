12edd9042d5725c96cfea23988f7c2824cbf0b26
ChuufMaster <ivanhorak20@gmail.com>
2024-07-31 22:07:29 +0200

    added neotest and stuff for dotnet testing

--- lua/ChuufMaster/plugins/debug.lua
+++ lua/ChuufMaster/plugins/debug.lua
@@ -25,10 +25,10 @@ return {
     'leoluz/nvim-dap-go',
   },
   config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    local dap = require('dap')
+    local dapui = require('dapui')
 
-    require('mason-nvim-dap').setup {
+    require('mason-nvim-dap').setup({
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_installation = true,
@@ -42,8 +42,9 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'netcoredbg',
       },
-    }
+    })
 
     -- Basic debugging keymaps, feel free to change to your liking!
     vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
@@ -52,12 +53,20 @@ return {
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
     vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<leader>B', function()
-      dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+      dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
     end, { desc = 'Debug: Set Breakpoint' })
+
+    local install_dir = require('mason-core.path').package_prefix('netcoredbg')
+    local netcoredbg_install_dir = install_dir .. '/netcoredbg'
+    dap.adapters.netcoredbg = {
+      type = 'executable',
+      command = netcoredbg_install_dir,
+      args = { '--interpreter=vscode -- dotnet' },
+    }
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
+    dapui.setup({
       -- Set icons to characters that are more likely to work in every terminal.
       --    Feel free to remove or use ones that you like more! :)
       --    Don't feel like these are good choices.
@@ -75,7 +84,7 @@ return {
           disconnect = '⏏',
         },
       },
-    }
+    })
 
     -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
     vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
@@ -85,12 +94,12 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup {
+    require('dap-go').setup({
       delve = {
         -- On Windows delve must be run attached or it crashes.
         -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
+        detached = vim.fn.has('win32') == 0,
       },
-    }
+    })
   end,
 }
---
+++ lua/ChuufMaster/plugins/neotest.lua
@@ -0,0 +1,34 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-neotest/nvim-nio',
+    'nvim-lua/plenary.nvim',
+    'antoinemadec/FixCursorHold.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'Issafalcon/neotest-dotnet',
+  },
+
+  config = function()
+    require('neotest').setup({
+      log_level = 1,
+      adapters = {
+        require('neotest-dotnet')({
+          dap = {
+            adapter_name = 'netcoredbg',
+          },
+          dotnet_additional_args = {
+            '-e ASPNETCORE_ENVIRONMENT=Development',
+          },
+        }),
+      },
+    })
+  end,
+  keys = {
+    {
+      '<leader>dt',
+      function()
+        require('neotest').run.run({ dotnet_additional_args = { '-e ASPNETCORE_ENVIRONMENT=Development', strategy = 'dap' } })
+      end,
+    },
+  },
+}
