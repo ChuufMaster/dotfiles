b6b5a191b42ca90f85c2e470fe0ae2d96971bbf6
ChuufMaster <ivanhorak20@gmail.com>
2024-08-07 09:29:59 +0200

    Added the java lsp

--- lua/ChuufMaster/plugins/lsp.lua
+++ lua/ChuufMaster/plugins/lsp.lua
@@ -5,6 +5,7 @@ return { -- LSP Configuration & Plugins
     { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'nvim-java/nvim-java',
 
     -- Useful status updates for LSP.
     -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
@@ -185,6 +186,24 @@ return { -- LSP Configuration & Plugins
       'stylua',
       'markdownlint', -- Used to format Lua code
     })
+    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
+    require('mason-lspconfig').setup({
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          -- This handles overriding only values explicitly passed
+          -- by the server configuration above. Useful when disabling
+          -- certain features of an LSP (for example, turning off formatting for tsserver)
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+        jdtls = function()
+          require('java').setup({})
+          require('lspconfig').jdtls.setup({})
+        end,
+      },
+    })
+
     require('lspconfig').racket_langserver.setup({})
     require('lspconfig').prolog_ls.setup({
       cmd = {
@@ -244,19 +263,6 @@ return { -- LSP Configuration & Plugins
           -- determining which version to use for project loading.
           IncludePrereleases = true,
         },
-      },
-    })
-    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
-    require('mason-lspconfig').setup({
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          -- This handles overriding only values explicitly passed
-          -- by the server configuration above. Useful when disabling
-          -- certain features of an LSP (for example, turning off formatting for tsserver)
-          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-          require('lspconfig')[server_name].setup(server)
-        end,
       },
     })
   end,
