b107f165fdcad89fbb57b44166778ebefd32e5b3
ChuufMaster <ivanhorak20@gmail.com>
2024-09-03 19:10:16 +0200

    Got wallpapers to work with selector when there are simlinks

--- scripts/scripts/wallpaper.sh
+++ scripts/scripts/wallpaper.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
 
-# Directory containing images
-# IMAGE_DIR="$HOME/.config/hypr/wallpapers"
-
-# Select two random images
-# IMAGE1=$(find "$IMAGE_DIR" -type f | shuf -n 1)
-# IMAGE2=$(find "$IMAGE_DIR" -type f | shuf -n 1)
-#
-# # Use the first image for DP-1
-# hyprctl hyprpaper preload "$IMAGE1"
-# hyprctl hyprpaper wallpaper "DP-1,$IMAGE1"
-#
-# # Use the second image for HDMI-A-1
-# hyprctl hyprpaper preload "$IMAGE2"
-# hyprctl hyprpaper wallpaper "HDMI-A-1,$IMAGE2"
-
-#!/bin/sh
-# Check swww daemon
 swww query
 if [ $? -eq 1 ] ; then
     swww init
 fi
-swww img ~/wallpapers/.current_wallpaper
+swww img ~/.current_wallpaper
