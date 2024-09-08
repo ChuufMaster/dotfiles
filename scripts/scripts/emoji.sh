cfafe7563cf24ad671d5b4d153e9fc2f67bc76e9
ChuufMaster <ivanhorak20@gmail.com>
2024-09-08 16:41:04 +0200

    Added emoji selectors for rofi

---
+++ scripts/scripts/emoji.sh
@@ -0,0 +1,9 @@
+rofi_output=$(rofi -emoji-format "{emoji}" \
+                   -emoji-mode stdout \
+                   -show emoji \
+                   -hover-select \
+                   -i \
+                   -theme $HOME/.config/rofi/themes/emoji.rasi)
+
+wtype "$rofi_output"
+wl-copy "$rofi_output"
---
+++ scripts/scripts/launcher.sh
@@ -0,0 +1,1 @@
+rofi -show drun -theme $HOME/.config/rofi/themes/launcher.rasi -hover-select -i
