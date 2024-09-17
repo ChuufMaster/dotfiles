25e80802adcfaeb78d930a7d3b8c0853b55050ff
ChuufMaster <ivanhorak20@gmail.com>
2024-09-17 11:44:27 +0200

    Hopefully fixed the PATH

--- zshrc/.config/zshrc/path.zsh
+++ zshrc/.config/zshrc/path.zsh
@@ -1,7 +1,13 @@
+typeset -gxU path PATH
 path+=(
-    /home/chuufmaster/.local/bin/
+    /home/chuufmaster/.local/bin
     /home/chuufmaster/.cargo/bin
     /home/chuufmaster/.dotnet/tools
     /opt/miktex/bin
     /usr/bin/flutter/bin
+    /usr/local/{sbin,bin}
+    /{sbin,bin}
+    $path
 )
+
+# export PATH
