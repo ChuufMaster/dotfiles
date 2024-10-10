9eea35f6b0a002dbcfa72d0eb63e265926f37d8c
ChuufMaster <ivanhorak20@gmail.com>
2024-10-10 10:57:29 +0200

    Added an indicator for when a macro is being recorded in neovim

--- nvim/.config/nvim/lua/ChuufMaster/plugins/lualine.lua
+++ nvim/.config/nvim/lua/ChuufMaster/plugins/lualine.lua
@@ -2,6 +2,30 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup({})
+    local function isRecording()
+      local reg = vim.fn.reg_recording()
+      if reg == '' then
+        return ''
+      end
+      return 'recording to ' .. reg
+    end
+    require('lualine').setup({
+      sections = {
+        lualine_a = { 'mode', { isRecording, color = 'Substitute' } },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' },
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { 'filename' },
+        lualine_x = { 'location' },
+        lualine_y = {},
+        lualine_z = {},
+      },
+    })
   end,
 }
