1af4babc3f792241dac7bf2527e870a8bdd43aba
ChuufMaster <ivanhorak20@gmail.com>
2024-10-10 10:57:04 +0200

    Added configs for folding using tree sitter

--- nvim/.config/nvim/lua/ChuufMaster/core/options.lua
+++ nvim/.config/nvim/lua/ChuufMaster/core/options.lua
@@ -61,12 +61,12 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '| ', trail = '·', nbsp = '␣' }
 
-vim.opt.tabstop = 4
+-- vim.opt.tabstop = 8
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
-vim.opt.smartindent = true
+-- vim.opt.smartindent = true
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -90,6 +90,15 @@ vim.opt.textwidth = 80
 vim.opt.formatoptions = {
   b = true,
 }
+
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldcolumn = '0'
+vim.opt.foldtext = ''
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 1
+vim.opt.foldnestmax = 4
+vim.opt.foldminlines = 3
 
 -- This file is automatically loaded by lazyvim.config.init.
 
