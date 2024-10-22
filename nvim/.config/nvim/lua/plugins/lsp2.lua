006a91f5d526aa2b9c72173d6936db37d03d1876
ChuufMaster <ivanhorak20@gmail.com>
2024-10-23 01:46:03 +0200

    Added some servers and configs for latex

--- nvim/.config/nvim/lua/plugins/lsp2.lua
+++ nvim/.config/nvim/lua/plugins/lsp2.lua
@@ -59,6 +59,99 @@ vim.api.nvim_create_autocmd("LspAttach", {
     callback = lsp_callback,
 })
 
+local path = vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+local words = {}
+
+for word in io.open(path, "r"):lines() do
+    table.insert(words, word)
+end
+
+local for_future = {
+    textlsp = {
+        settings = {
+            analysers = {
+                --[[ languagetool = {
+                    enabled = true,
+                    check_text = {
+                        on_open = true,
+                        on_save = true,
+                        on_change = false,
+                    },
+                }, ]]
+                --[[ ollama = {
+                    enabled = true,
+                    check_text = {
+                        on_open = false,
+                        on_save = true,
+                        on_change = false,
+                    },
+                    model = "phi3:3.8b-instruct", -- smaller but faster model
+                    -- model = "phi3:14b-instruct",  -- more accurate
+                    max_token = 50,
+                }, ]]
+                gramformer = {
+                    -- gramformer dependency needs to be installed manually
+                    enabled = false,
+                    gpu = false,
+                    check_text = {
+                        on_open = false,
+                        on_save = true,
+                        on_change = false,
+                    },
+                },
+                hf_checker = {
+                    enabled = false,
+                    gpu = false,
+                    quantize = 32,
+                    model = "pszemraj/flan-t5-large-grammar-synthesis",
+                    min_length = 40,
+                    check_text = {
+                        on_open = false,
+                        on_save = true,
+                        on_change = false,
+                    },
+                },
+                hf_instruction_checker = {
+                    enabled = false,
+                    gpu = false,
+                    quantize = 32,
+                    model = "grammarly/coedit-large",
+                    min_length = 40,
+                    check_text = {
+                        on_open = false,
+                        on_save = true,
+                        on_change = false,
+                    },
+                },
+                hf_completion = {
+                    enabled = false,
+                    gpu = false,
+                    quantize = 32,
+                    model = "bert-base-multilingual-cased",
+                    topk = 5,
+                },
+            },
+            documents = {
+                -- the language of the documents, could be set to `auto` of `auto:<fallback>`
+                -- to detect automatically, default: auto:en
+                language = "auto:en",
+                -- do not autodetect documents with fewer characters
+                min_length_language_detect = 20,
+                org = {
+                    org_todo_keywords = {
+                        "TODO",
+                        "IN_PROGRESS",
+                        "DONE",
+                    },
+                },
+                txt = {
+                    parse = true,
+                },
+            },
+        },
+    },
+}
+
 local servers = {
     marksman = {
         settings = { filetypes = { "markdown" } },
@@ -75,6 +168,18 @@ local servers = {
     docker_compose_language_service = {},
     sqlls = {
         filetypes = { "sql" },
+    },
+    ltex = {
+        settings = {
+            ltex = {
+                -- language = "en",
+                dictionary = {
+                    ["en"] = words,
+                    ["en-US"] = words,
+                    ["en-GB"] = words,
+                },
+            },
+        },
     },
     lua_ls = {
         -- cmd = {...},
@@ -118,15 +223,20 @@ return {
 
         require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
-        mason_lspconfig.setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({
-                    capabilities = capabilities,
-                    on_attach = on_attach,
-                    settings = servers[server_name],
-                    filetypes = (servers[server_name] or {}).filetypes,
-                })
-            end,
+        require("mason-lspconfig").setup({
+            handlers = {
+                function(server_name)
+                    local server = servers[server_name] or {}
+                    server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+                    require("lspconfig")[server_name].setup(server)
+                    --[[ require("lspconfig")[server_name].setup({
+                        capabilities = capabilities,
+                        on_attach = on_attach,
+                        settings = servers[server_name],
+                        filetypes = (servers[server_name] or {}).filetypes,
+                    }) ]]
+                end,
+            },
         })
 
         -- require("lspconfig").jdtls.setup({})
