0a4c93c21092c37247af58388244649fcb53d9d0
ChuufMaster <ivanhorak20@gmail.com>
2024-10-23 01:43:37 +0200

    Added a function for selecting a random figlet font

--- zshrc/.config/zshrc/functions.zsh
+++ zshrc/.config/zshrc/functions.zsh
@@ -1,2 +1,17 @@
 # vim: filetype=sh
 
+function y()
+{
+    local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    yazi "$@" --cwd-file="$tmp"
+    if cwd="$(command cat -- "$tmp")" && [ 0n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+        builtin cd -- "$cwd"
+    fi
+    rm -f -- "$tmp"
+}
+
+function randomfiglet()
+{
+    font="$(basename -s .flf "$(ls /usr/share/figlet/fonts/*.flf | shuf -n 1)")"
+    figlet -cWp -f "$font" "$@"
+}
