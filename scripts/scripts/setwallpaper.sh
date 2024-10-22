2e618b5f7567416a0807fca701da94daa6b35d1a
ChuufMaster <ivanhorak20@gmail.com>
2024-10-23 01:41:55 +0200

    Added pywal integration for webcord and zathura using the new pywal16

--- scripts/scripts/setwallpaper.sh
+++ scripts/scripts/setwallpaper.sh
@@ -9,35 +9,49 @@ CHOSEN="$1"
 
 executeCommand() {
 
-  if command -v swww &>/dev/null; then
-    swww img "$1" ${SWWW_PARAMS}
+    if command -v swww &>/dev/null; then
+        swww img "$1" ${SWWW_PARAMS}
 
-  elif command -v swaybg &>/dev/null; then
-    swaybg -i "$1" &
-  
-  else
-    echo "Neither swww nor swaybg are installed."
-    exit 1
-  fi
+    elif command -v swaybg &>/dev/null; then
+        swaybg -i "$1" &
 
-  ln -sf "$1" "$HOME/.current_wallpaper"
+    else
+        echo "Neither swww nor swaybg are installed."
+        exit 1
+    fi
+
+    ln -sf "$1" "$HOME/.current_wallpaper"
+    cp "$1" "$HOME/.mozilla/firefox/1opdykuv.default-release-1729612380706/chrome/current_wallpaper.jpg"
+    # ln -sf "$1" "$HOME/.mozilla/firefox/1opdykuv.default-release-1729612380706//chrome/current_wallpaper.jpg"
 }
 
-pywal(){
+pywal() {
 
-  wallpaper_path=$(readlink "$HOME/.current_wallpaper")
+    wallpaper_path=$(readlink "$HOME/.current_wallpaper")
 
-  check_file "$wallpaper_path"
+    check_file "$wallpaper_path"
 
-  wal -i "$wallpaper_path" -s -t
+    wal -n -i "$wallpaper_path" -s -t
 
-  # Update sway config files and css
-  swaync-client --reload-css
-  swaync-client --reload-config
+    # Update sway config files and css
+    swaync-client --reload-css
+    swaync-client --reload-config
 
-  # Update thunderbird using pywalfox
-  pywalfox update
+    # Update thunderbird using pywalfox
+    pywalfox update
+}
+
+zathura_update() {
+    genzathurarc >>$HOME/.config/zathura/zathurarc
+}
+
+webcord_update() {
+    webcord_config=~/.config/WebCord/Themes
+    pywal-discord -p $webcord_config
+    mv $webcord_config/pywal-discord-default.theme.css $webcord_config/pywal-discord-default.theme
 }
 
 executeCommand "$CHOSEN"
 pywal
+zathura_update
+webcord_update
