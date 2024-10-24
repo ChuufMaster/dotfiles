71bbec8455046e125c913220cf70cf0920b17b31
ChuufMaster <ivanhorak20@gmail.com>
2024-10-24 21:48:20 +0200

    💾 Neovim: Added keys for rezizing window

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -29,6 +29,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -5<CR>", {desc = "Decrease Window Width"})
+vim.keymap.set("n", "<C-Down>", "<cmd>resize -5<CR>", {desc = "Decrease Window Height"})
+vim.keymap.set("n", "<C-Up>", "<cmd>resize +5<CR>", {desc = "Increase Window Height"})
+vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +5<CR>", {desc = "Increase Window Width"})
 -- Vim remap overides
 
 vim.keymap.set("i", "<C-b>", "<ESC>^i", { desc = "Beginning of line" })
