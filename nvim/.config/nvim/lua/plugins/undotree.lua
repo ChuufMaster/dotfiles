1dbd35136c8a1864d9911f5355a55c5c93a076c3
ChuufMaster <ivanhorak20@gmail.com>
2024-10-24 21:56:53 +0200

    💾 Neovim: `undotree` now gets propery lazy loaded by using a command

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,7 +1,13 @@
 return {
-  'mbbill/undotree',
-
-  config = function()
-    vim.keymap.set('n', '<leader>tu', vim.cmd.UndotreeToggle)
-  end,
+    "mbbill/undotree",
+    cmd = "UndotreeToggle",
+    keys = {
+        "<leader>tu",
+        "UndotreeToggle",
+        desc = "[T]oggle [U]ndotree",
+    },
+    opts = {},
+    -- config = function()
+    --     vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle)
+    -- end,
 }
