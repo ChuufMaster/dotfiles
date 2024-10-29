3f236f3ed9a97af5fc0b78b66b8dbeb60c5b5c8c
ChuufMaster <ivanhorak20@gmail.com>
2024-10-30 01:45:06 +0200

    💲 Shell: Used a clever trick for setting the color scheme in yazi

--- zshrc/.config/zshrc/functions.zsh
+++ zshrc/.config/zshrc/functions.zsh
@@ -3,6 +3,7 @@
 function y()
 {
     local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+    cat ~/.cache/wal/sequences
     yazi "$@" --cwd-file="$tmp"
     if cwd="$(command cat -- "$tmp")" && [ 0n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
         builtin cd -- "$cwd"
