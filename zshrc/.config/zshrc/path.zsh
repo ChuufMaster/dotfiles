8e19ab07fa310c1235452140699afdcf88f3b8f3
ChuufMaster <ivanhorak20@gmail.com>
2024-10-30 01:44:32 +0200

    💲 Shell: let archlinux control jvm path

--- zshrc/.config/zshrc/path.zsh
+++ zshrc/.config/zshrc/path.zsh
@@ -1,7 +1,7 @@
 typeset -gxU path PATH
 
 export ANDROID_HOME=$HOME/android-sdk
-export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
+# export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 
 path+=(
     /home/chuufmaster/.local/bin
