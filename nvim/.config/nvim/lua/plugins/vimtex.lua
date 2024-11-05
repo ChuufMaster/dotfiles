4528603de339e136c63e5ab6c6dfc58e646e8057
ChuufMaster <ivanhorak20@gmail.com>
2024-11-05 21:23:08 +0200

    📝 Nvim: Configured vimtex compiler

--- nvim/.config/nvim/lua/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/plugins/vimtex.lua
@@ -6,6 +6,22 @@ return {
     init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = "zathura"
+        -- vim.g.vimtex_compiler_latexmk_engines = { ["_"] = "-lualatex" }
         vim.g.vimtex_format_enabled = true
+        vim.g.vimtex_compiler_latexmk = {
+            callback = 1,
+            continuous = 1,
+            executable = "latexmk",
+            -- hooks = {},
+            -- aux_dir = ".build",
+            -- out_dir = "",
+            options = {
+                "-shell-escape",
+                "-verbose",
+                "-file-line-error",
+                "-synctex=1",
+                "-interaction=nonstopmode",
+            },
+        }
     end,
 }
