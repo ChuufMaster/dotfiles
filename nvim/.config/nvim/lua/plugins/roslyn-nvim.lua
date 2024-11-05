20107f05749856c781376cada9454c590cfbc977
ChuufMaster <ivanhorak20@gmail.com>
2024-11-05 21:22:09 +0200

    📝 Nvim: Configured roslyn a bit better

--- nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
+++ nvim/.config/nvim/lua/plugins/roslyn-nvim.lua
@@ -1,7 +1,51 @@
 return {
-  'seblj/roslyn.nvim',
-  ft = 'cs',
-  opts = {
-    -- your configuration comes here; leave empty for default settings
-  },
+    "seblj/roslyn.nvim",
+    ft = "cs",
+    -- opts = {},
+    -- dev = true,
+    -- init = function()
+    --     vim.cmd("!source ~/.config/zshrc/functions.zsh; randomfiglet yes")
+    -- end,
+    opts = function()
+        -- local update_roslyn = io.popen([[echo 1]])
+        -- local isrunning = update_roslyn:read("*a")
+        -- update_roslyn:close()
+        return {
+            config = {
+                settings = {
+                    ["csharp|background_analysis"] = {
+                        dotnet_analyzer_diagnostics_scope = "fullSolution",
+                        dotnet_compiler_diagnostics_scope = "fullSolution",
+                    },
+
+                    ["csharp|code_lens"] = {
+                        dotnet_enable_references_code_lens = true,
+                        dotnet_enable_tests_code_lens = true,
+                    },
+                    ["csharp|completion"] = {
+                        dotnet_provide_regex_completions = true,
+                        dotnet_show_completion_items_from_unimported_namespaces = true,
+                        dotnet_show_name_completion_suggestions = true,
+                    },
+                    ["csharp|inlay_hints"] = {
+                        csharp_enable_inlay_hints_for_implicit_object_creation = true,
+                        csharp_enable_inlay_hints_for_implicit_variable_types = true,
+                        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+                        csharp_enable_inlay_hints_for_types = true,
+                        dotnet_enable_inlay_hints_for_indexer_parameters = true,
+                        dotnet_enable_inlay_hints_for_literal_parameters = true,
+                        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+                        dotnet_enable_inlay_hints_for_other_parameters = true,
+                        dotnet_enable_inlay_hints_for_parameters = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+                        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+                    },
+                    ["csharp|symbol_search"] = {
+                        dotnet_search_reference_assemblies = true,
+                    },
+                },
+            },
+        }
+    end,
 }
