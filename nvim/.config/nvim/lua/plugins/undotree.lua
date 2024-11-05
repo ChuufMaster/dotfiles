3788faea7ebce946a449288cd372e9fc54bbeb3c
ChuufMaster <ivanhorak20@gmail.com>
2024-11-05 21:22:40 +0200

    📝 Nvim: Fixed toggling undo tree

--- nvim/.config/nvim/lua/plugins/undotree.lua
+++ nvim/.config/nvim/lua/plugins/undotree.lua
@@ -1,13 +1,14 @@
 return {
     "mbbill/undotree",
     cmd = "UndotreeToggle",
+    lazy = false,
     keys = {
         "<leader>tu",
         "UndotreeToggle",
         desc = "[T]oggle [U]ndotree",
     },
     opts = {},
-    -- config = function()
-    --     vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle)
-    -- end,
+    config = function()
+        vim.keymap.set("n", "<leader>tu", vim.cmd.UndotreeToggle, { desc = "[T]oggle [U]ndotree" })
+    end,
 }
