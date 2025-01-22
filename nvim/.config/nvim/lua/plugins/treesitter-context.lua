a6a51312945d194ca38f8198aeb1c22e96d2bb4c
ChuufMaster <ivanhorak20@gmail.com>
2025-01-22 15:03:53 +0200

    📝 Nvim: Disabled treesitter objects

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -1,10 +1,11 @@
 return {
-  'nvim-treesitter/nvim-treesitter-context',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  config = function()
-    require('treesitter-context').setup {
-      max_lines = 5,
-      multiline_threshold = 1,
-    }
-  end,
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false,
+    config = function()
+        require("treesitter-context").setup({
+            max_lines = 5,
+            multiline_threshold = 1,
+        })
+    end,
 }
