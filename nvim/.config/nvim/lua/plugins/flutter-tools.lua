e9a5409e21d7ac44ce4c22cd7abb080b29cd50b5
ChuufMaster <ivanhorak20@gmail.com>
2025-02-11 09:09:58 +0200

    📝 Nvim: disabled flutter-tools

--- nvim/.config/nvim/lua/plugins/flutter-tools.lua
+++ nvim/.config/nvim/lua/plugins/flutter-tools.lua
@@ -1,12 +1,16 @@
 return {
-  'akinsho/flutter-tools.nvim',
-  ft = { 'dart' },
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'stevearc/dressing.nvim', -- optional for vim.ui.select
-  },
-opts = function()
+    "akinsho/flutter-tools.nvim",
+    ft = { "dart" },
+    enabled = false,
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    opts = function()
         pcall(require("telescope").load_extension, "flutter")
     end,
-  config = true,
+    config = true,
+    keys = {
+        { "<leader>fF", "<cmd>Telescope flutter commands<CR>", desc = "[F]ind [F]lutter commands" },
+    },
 }
