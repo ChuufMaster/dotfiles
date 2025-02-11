e3fab81ad9b194bc2e56a5955ee86790a7061598
ChuufMaster <ivanhorak20@gmail.com>
2025-02-11 09:06:50 +0200

    📝 Nvim: Removed some unused plugins

--- nvim/.config/nvim/lua/plugins/luarocks-nvim.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "vhyrro/luarocks.nvim",
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    opts = {
-        rocks = { "magick" },
-    },
-}
--- nvim/.config/nvim/lua/plugins/vim-table-mode.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'dhruvasagar/vim-table-mode',
-  enabled = false,
-
-  lazy = true,
-}
