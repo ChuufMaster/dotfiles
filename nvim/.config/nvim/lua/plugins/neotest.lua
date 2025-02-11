dda66b0e9d728eff728c0c7e6e92a1ba031f9e57
ChuufMaster <ivanhorak20@gmail.com>
2025-02-11 09:09:21 +0200

    📝 Nvim: Formatting

--- nvim/.config/nvim/lua/plugins/neotest.lua
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -1,34 +1,38 @@
 return {
-  'nvim-neotest/neotest',
-  dependencies = {
-    'nvim-neotest/nvim-nio',
-    'nvim-lua/plenary.nvim',
-    'antoinemadec/FixCursorHold.nvim',
-    'nvim-treesitter/nvim-treesitter',
-    'Issafalcon/neotest-dotnet',
-  },
-
-  config = function()
-    require('neotest').setup({
-      log_level = 1,
-      adapters = {
-        require('neotest-dotnet')({
-          dap = {
-            adapter_name = 'netcoredbg',
-          },
-          dotnet_additional_args = {
-            '-e ASPNETCORE_ENVIRONMENT=Development',
-          },
-        }),
-      },
-    })
-  end,
-  keys = {
-    {
-      '<leader>dt',
-      function()
-        require('neotest').run.run({ dotnet_additional_args = { '-e ASPNETCORE_ENVIRONMENT=Development', strategy = 'dap' } })
-      end,
+    "nvim-neotest/neotest",
+    dependencies = {
+        "nvim-neotest/nvim-nio",
+        "nvim-lua/plenary.nvim",
+        "antoinemadec/FixCursorHold.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        "Issafalcon/neotest-dotnet",
     },
-  },
+
+    config = function()
+        ---@diagnostic disable-next-line: missing-fields
+        require("neotest").setup({
+            log_level = 1,
+            adapters = {
+                require("rustaceanvim.neotest"),
+                require("neotest-dotnet")({
+                    dap = {
+                        adapter_name = "netcoredbg",
+                    },
+                    dotnet_additional_args = {
+                        "-e ASPNETCORE_ENVIRONMENT=Development",
+                    },
+                }),
+            },
+        })
+    end,
+    keys = {
+        {
+            "<leader>dt",
+            function()
+                require("neotest").run.run({
+                    dotnet_additional_args = { "-e ASPNETCORE_ENVIRONMENT=Development", strategy = "dap" },
+                })
+            end,
+        },
+    },
 }
