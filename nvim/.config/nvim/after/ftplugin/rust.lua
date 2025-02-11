47ac70281b0cb93675290dbe075094659815a641
ChuufMaster <ivanhorak20@gmail.com>
2025-02-11 09:02:54 +0200

    📝 Nvim: Added some rust configs

--- nvim/.config/nvim/after/ftplugin/rust.lua
+++ nvim/.config/nvim/after/ftplugin/rust.lua
@@ -1,3 +1,9 @@
+local bufnr = vim.api.nvim_get_current_buf()
+
+vim.keymap.set("n", "<leader>ra", function()
+    vim.cmd.RustLsp("codeAction")
+end, { silent = true, buffer = bufnr, desc = "[R]ust code[A]ction" })
+
 vim.keymap.set(
     "n",
     "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
@@ -6,3 +12,7 @@ vim.keymap.set(
     end,
     { silent = true, buffer = bufnr }
 )
+
+vim.keymap.set("n", "<leader>rd", function()
+    require("dap").continue()
+end, { silent = true, buffer = bufnr, desc = "[R]ust [D]ebug" })
--- nvim/.config/nvim/lua/plugins/rust.lua
+++ nvim/.config/nvim/lua/plugins/rust.lua
@@ -2,6 +2,7 @@ return {
     {
         "mrcjkb/rustaceanvim",
         version = "^5", -- Recommended
+        ft = "rust",
         lazy = false, -- This plugin is already lazy
     },
 }
